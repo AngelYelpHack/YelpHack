bb97d649af91e04c7e2fd49eba6328282c996049
PGilbertSchmitt <PGilbertSchmitt@gmail.com>
2017-07-29 16:31:07 -0700

    Create simple frontpage component

---
+++ frontend/src/components/frontpage.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Frontpage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Where ya headed?</h1>
+            </div>
+        )
+    }
+}
