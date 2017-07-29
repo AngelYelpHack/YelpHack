068726fcdc5a031c314ac112f79313d74f64040f
amatranga <amatranga892@gmail.com>
2017-07-29 14:13:59 -0700

    [Feat] OAuth
    [x]Implement serialization/deserialization of users
    [x]Implement Google OAuth strategy

---
+++ server/middleware/passport.js
@@ -0,0 +1,34 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const GithubStrategy = require('passport-github').Strategy;
+const models = require('../../db/models');
+
+passport.serializeUser((profile, done) => {
+  done(null, profile.id);
+});
+
+passport.deserializeUser((id, done) => {
+  return models.Profile.where({ id }).fetch()
+    .then(profile => {
+      if(!profile) {
+        throw profile;
+      }
+      done(null, profile.serialize());
+    })
+    .error(error => {
+      done(error, null);
+    })
+    .catch(() => {
+      done(null, null, { message: 'No user found' });
+    });
+});
+
+passport.use('google', new GoogleStrategy({
+  clientId: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: process.env.GOOGLE_CALLBACKURL
+},
+  (accessToken, refreshToken, profile, done) => getOrCreateOAuthProfile('google', profile, done))
+);
+
