f39dd888e88a65afe387e9d9a2205afbf95c642e
amatranga <amatranga892@gmail.com>
2017-07-30 11:18:24 -0700

    PUSH ALL THE THINGS!

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,8 +1,10 @@
 'use strict';
 
-module.exports = function(server) {
-  // Install a `/` route that returns server status
-  var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
-  server.use(router);
-};
+// const path = require('path');
+// const serve = path.join(__dirname, '../../client/index.html')
+
+// module.exports = (app) => {
+//   app.get('/*', (req, res) => {
+//     res.sendFile(serve);
+//   });
+// }
