d945c115747702f9eb7fc2951b4aef1f6dddd14c
PGilbertSchmitt <PGilbertSchmitt@gmail.com>
2017-07-30 12:56:03 -0700

    Remove extension

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -10,7 +10,7 @@ import {
 
 import store from './store/store';
 import Login from './components/login.jsx';
-import Frontpage from './components/frontpage-container.jsx';
+import Frontpage from './components/frontpage-container';
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
