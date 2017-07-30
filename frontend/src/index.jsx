dfa126696a1581392a6bbb4a719c747678697dbb
Russ Ward <maddogward@gmail.com>
2017-07-29 17:28:40 -0700

    React router setup

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import store from './store/store';
+import Login from './components/login.jsx';
+//import Frontpage from './frontpage.jsx';
 
 document.addEventListener("DOMContentLoaded", () => {
-    ReactDOM.render(<h1>Beep!</h1>, document.getElementById("root"));
+  ReactDOM.render(
+    <Provider store={store} >
+      <BrowserRouter>
+        <div>
+          <Switch>
+            <Route path="/" component={Login} />>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </Provider>
+    , document.getElementById("root"));
 });
 
 // Only for testing
