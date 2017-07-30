f429e1d730541037b94f01bc2e410a2e2bbe709e
PGilbertSchmitt <PGilbertSchmitt@gmail.com>
2017-07-29 17:05:12 -0700

    Add geosuggest

--- frontend/src/components/frontpage.jsx
+++ frontend/src/components/frontpage.jsx
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
+import Geosuggest from 'react-geosuggest';
 
 class Frontpage extends Component {
     constructor(props) {
         super(props);
     }
 
+    speak(data) {
+        console.log(data)
+    }
+
     render() {
         return (
             <div>
-                <h1>Where ya headed?</h1>
+                <div>
+                    <h1>Find your poison and drink it.</h1>
+                </div>
+                
+                <div>
+                    <Geosuggest 
+                        placeholder="Where are ya headed?"
+                        onSuggestSelect={data => this.speak(data)}
+                    />
+                </div>
             </div>
         )
     }
